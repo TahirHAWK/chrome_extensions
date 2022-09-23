2cea87e7e709cdbf0ee9d17c767b22c1a46a5575
MarufIUBAT <marufhossain1188@gmail.com>
2022-09-23 22:30:43 +0600

    all my extension projects will be added here from now on

---
+++ multiple links copier for ICC server/test.js
@@ -0,0 +1,6 @@
+// document.getElementsByClassName('dropdown-menu')[5].getElementsByTagName('a')[0].href
+
+// getting an href tag using js
+
+// task 1: print each line of file download link from the web page with one line break between each link
+// task 2: copy those links that was fetched previously using javascript
