00a592453684c867eab2c32ada94a3ce1d127c3d
TahirHAWK <tahirtamin20@gmail.com>
2023-07-30 13:10:38 -0700

    designed the popup html with css

--- YT German Caption monitor extension/popup.js
+++ YT German Caption monitor extension/popup.js
@@ -1,3 +1,24 @@
 // popup.js
-console.log(germanWordList)
+const url = 'https://word-translation-word-to-word.p.rapidapi.com/translatewordtoword';
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'X-RapidAPI-Key': 'ba1a7f5c75mshbcedc7b3b03e1e6p19097bjsn46db42b0db40',
+		'X-RapidAPI-Host': 'word-translation-word-to-word.p.rapidapi.com'
+	},
+	body: new URLSearchParams({
+		word: 'Mutter',
+		fromlanguage: 'German',
+		tolanguage: 'English'
+	})
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result, result.data);
+} catch (error) {
+	console.error(error);
+}
 // Empty popup script for Manifest V3
