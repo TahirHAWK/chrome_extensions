c3f74d6ea2d3f65a2356ee24f931be6c0ce11720
TahirHAWK <tahirtamin20@gmail.com>
2023-08-20 14:20:24 -0700

    now it can fetch channel logo link.

--- YT German Caption monitor extension/allCaptionCollectorContentScript.js
+++ YT German Caption monitor extension/allCaptionCollectorContentScript.js
@@ -28,14 +28,26 @@ function extractWords(str) {
     return words;
   }
 
+function extractChannelLogo(){
+  let imageSources = []
+  let images = document.getElementsByTagName('img')
+
+  for(i=0; i<images.length; i++){
+    if(images[i].src.includes('yt3.ggpht.com')){
+        imageSources.push(images[i].src)
+    }
+}
+  return imageSources[1]
+}
+
 
 
 
 // listen for requests from the popup js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-  
+   
     // must declare these inside in order to work
+    let channelLogoLink = extractChannelLogo()
     let channelName = document.getElementsByClassName('yt-simple-endpoint style-scope yt-formatted-string')[0].innerText
     let channelLink = document.getElementsByClassName('yt-simple-endpoint style-scope yt-formatted-string')[0].href
     let videoTitle =document.getElementsByTagName('h1')[1].innerText
@@ -49,9 +61,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             console.log('detected german successfully..')
              allCaptions = allCaptionsTab.innerText  
             allWords = extractUniqueWords(extractWords(allCaptions)).sort()
-            console.log(extractWords(allCaptions).length, extractUniqueWords(allCaptions).length,extractUniqueWords(extractWords(allCaptions)).length)
+            console.log(extractWords(allCaptions).length, extractUniqueWords(allCaptions).length, extractUniqueWords(extractWords(allCaptions)).length)
             // make the object of the video details
-            videoDetails = { title: videoTitle.trim(), channel: channelName.trim(), url: window.location.href.trim(), channelUrl: channelLink.trim(), captionWords: allWords}
+            videoDetails = { title: videoTitle.trim(), channel: channelName.trim(), url: window.location.href.trim(), channelUrl: channelLink.trim(), captionWords: allWords, channelLogoLink: channelLogoLink}
             sendResponse(videoDetails);
     } else{
       console.log('Some problems occured:', captionTabExists, request.message == "request all yt captions")
