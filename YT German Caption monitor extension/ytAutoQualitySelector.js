422902f3aa9aadb4d797d07bae5de6210098c694
TahirHAWK <tahirtamin20@gmail.com>
2024-01-11 10:17:27 +0600

    made some updates which will work in future, on clicking automatically.

--- YT German Caption monitor extension/ytAutoQualitySelector.js
+++ YT German Caption monitor extension/ytAutoQualitySelector.js
@@ -1,37 +1,46 @@
 // automatically selects 480p as default quality
 let lastURL = [window.location.href]
 
+function clickPromise(element) {
+  return new Promise((resolve) => {
+   if (!element) {
+     console.error('Element not found');
+     return;
+   }
+   setTimeout(() => {
+     element.click();
+     resolve();
+   }, 100);
+  });
+ }
+  
+ 
+
 // .getAttribute('aria-expanded'), || qualityButtons[i].innerText.includes('720')
 // declaration of different functions
 
 // open the 'show transcript' section
 function openShowTranscriptSection(){
-  // old transcript button exposer
-
-  // return new Promise((resolve, reject)=>{
-  //   let moreOptions = document.getElementsByClassName('yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-button ')[1]
-  //   moreOptions.click()
-  //   setTimeout(()=>{
-  //     // click on the 'show transcript' button
-  //     document.getElementsByTagName('ytd-menu-service-item-renderer')[3].click()
-  //     resolve()
-  //   }, 250)
-  // })
-
-
   // new transcript button exposer
 
     return new Promise((resolve, reject)=>{
-    let clickOnMore = document.getElementsByTagName('tp-yt-paper-button')[2]
-    clickOnMore.click()
-    console.log('clicked on more')
+      console.log('Inside the openShowTranscriptSection')
+      let clickOnMore = document.getElementsByTagName('tp-yt-paper-button')[2]
+      clickPromise(clickOnMore).then(() => {
+      console.log('clicked on more')
+      setTimeout(()=>{
+        // click on the 'show transcript' button
+        let showTranscriptButton = document.getElementsByTagName('ytd-button-renderer')[4]
+        clickPromise(showTranscriptButton).then(()=>{
+          console.log('clicked on show transcript')
+          resolve()
+        })
+      }, 250)
+    }).catch(()=>{
+      console.log('Didnt work : the click promise')
+      reject()
+    })
 
-    setTimeout(()=>{
-      // click on the 'show transcript' button
-      document.getElementsByTagName('ytd-button-renderer')[4].click()
-      console.log('clicked on show transcript')
-      resolve()
-    }, 250)
   })
 }
 
@@ -40,8 +49,10 @@ function select720p(){
   let qualityButtons = document.getElementsByClassName('ytp-menuitem-label')
   for(i=0; i<qualityButtons.length; i++){
       if(qualityButtons[i].innerText.includes('720') ){
-        qualityButtons[i].click()
-        i = qualityButtons.length
+        clickPromise(qualityButtons[i]).then(()=>{
+          i = qualityButtons.length
+          console.log('selected 720p using clickPromise.')
+        })
       }
   }
 
@@ -56,9 +67,17 @@ function checkSettingsButtonClickStatus(){
 
 // sets the video quality to 480p
 function set720p(){
-  document.getElementsByClassName('ytp-button ytp-settings-button')[0].click()
-  document.getElementsByClassName('ytp-menu-label-secondary')[0].click()
-  setTimeout(select720p, 1000)
+  clickPromise(document.getElementsByClassName('ytp-button ytp-settings-button')[0]).then(()=>{
+    console.log('successfully clicked on settings button')
+    setTimeout(clickPromise(document.getElementsByClassName('ytp-menu-label-secondary')[0])
+    .then(()=>{
+      
+      console.log('successfully clicked on quality button')
+      setTimeout(select720p, 1000)
+    }
+    ), 100)
+  
+  })
 }
 
 function checkIfVideoLinkChanged(e){
@@ -78,23 +97,18 @@ function checkIfVideoLinkChanged(e){
 
 // calling of the functions
 // global area, only works if the url is from youtube
-if(window.location.href.includes('youtube.com/watch')){
-  // checking if the site we are on is youtube or not
 
-   
-
-    //   declaring the function after loading first time
-    
-
-}
 
 window.addEventListener('keydown', function(event) {
   if (event.key.toLowerCase() == 'q') {
    console.log('Q key pressed');
-   // Add your code here
-   setTimeout(set720p, 1000)
-   setTimeout(openShowTranscriptSection, 3000)
-   setInterval(checkIfVideoLinkChanged, 10*1000);
+   // checking if the site we are on is youtube or not
+   if(window.location.href.includes('youtube.com/watch')){
+    //   declaring the function after loading first time
+    setTimeout(set720p, 1000)
+    setTimeout(openShowTranscriptSection, 3000)
+    setInterval(checkIfVideoLinkChanged, 10*1000);
+  }
   }
  });
 
